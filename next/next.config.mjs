17b236e4278366ac7f48c8d5242a10c5f223863d
Takuya Sugiyama (@motojouya) <motojouya@gmail.com>
2024-04-26 13:57:35 +0000

    tsc

--- next/next.config.mjs
+++ next/next.config.mjs
@@ -16,3 +16,6 @@ export default nextConfig;
 // graphql-codegen入れたらでてきた。なんか必要かも
 // npm WARN deprecated @babel/plugin-proposal-class-properties@7.18.6: This proposal has been merged to the ECMAScript standard and thus this plugin is no longer maintained. Please use @babel/plugin-transform-class-properties instead.
 // npm WARN deprecated @babel/plugin-proposal-object-rest-spread@7.20.7: This proposal has been merged to the ECMAScript standard and thus this plugin is no longer maintained. Please use @babel/plugin-transform-object-rest-spread instead.
+//
+// tsconfig.jsonはcreate-next-appした状態から変えてないので、たぶんなんか必要そう。
+// tcs-aliasもいれてないが、cjsでコンパイルする気がするので、不要な気がする。
